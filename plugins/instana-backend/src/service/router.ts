4c496fb5378b2f29f3a84244a1e677aa4d2b7ae7
Chris Hawkins <cmdkeen@users.noreply.github.com>
2023-04-10 15:00:35 +0100

    Shape of web and app metrics
    Signed-off-by: Chris Hawkins <cmdkeen@users.noreply.github.com>

--- plugins/instana-backend/src/service/router.ts
+++ plugins/instana-backend/src/service/router.ts
@@ -14,26 +14,53 @@
  * limitations under the License.
  */
 import { errorHandler } from '@backstage/backend-common';
+import { Config } from '@backstage/config';
 import express from 'express';
 import Router from 'express-promise-router';
 import { Logger } from 'winston';
 
 export interface RouterOptions {
   logger: Logger;
+  config: Config;
 }
 
 export async function createRouter(
   options: RouterOptions,
 ): Promise<express.Router> {
-  const { logger } = options;
-
   const router = Router();
   router.use(express.json());
 
   router.get('/health', (_, response) => {
-    logger.info('PONG!');
     response.json({ status: 'ok' });
   });
+
+  router.get('/applications/:applicationId', async (req, res) => {
+    const { applicationId } = req.params;
+    const metrics = {
+      applicationId: applicationId,
+      windowSize: 84600000,
+      'calls.per_second': 5.831666666666667,
+      'latency.mean': 5.171477565018577,
+      'latency.p50': 0.0,
+      'latency.p90': 0.0,
+      'latency.p99': 38.0,
+    };
+    res.json(metrics);
+  });
+
+  router.get('/websites/:websiteId', async (req, res) => {
+    const { websiteId } = req.params;
+    const metrics = {
+      websiteId: websiteId,
+      windowSize: 84600000,
+      'onLoadTime.p90': 949.0,
+      'onLoadTime.p50': 415.0,
+      'firstContentfulPaintTime.p90': 539.0,
+      'firstContentfulPaintTime.p50': 302.0,
+    };
+    res.json(metrics);
+  });
+
   router.use(errorHandler());
   return router;
 }
